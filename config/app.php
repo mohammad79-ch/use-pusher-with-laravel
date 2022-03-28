131f81317be79b2bd8b94285aca86050fa6dd16d
mamado <mamadbosqets@gmail.com>
2022-03-28 15:09:59 +0430

    send the notifications to all user that authentecated for logined and logout

--- config/app.php
+++ config/app.php
@@ -171,7 +171,8 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+         App\Providers\BroadcastServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
